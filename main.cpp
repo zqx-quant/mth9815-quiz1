ade34c948cca31223e8bc63cf62ef09be31489d2
zqx-quant <67666795+zqx-quant@users.noreply.github.com>
2022-11-22 19:00:32 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,75 @@
 #include <iostream>
+#include <vector>
+#include <iostream>
+
 using namespace std;
 
-void QuickSort(int *arr, int start, int end)
-{
-    if (start >= end)
-        return;
-    int i = start;
-    int key = arr[i];
-    int j = end;
-    while (i < j)
-    {
-        while (i < j && key <= arr[j])
-        {
-            j--;
-        }
-        if (i < j)
-            arr[i++] = arr[j];
-        while (i < j && key >= arr[i])
-        {
-            i++;
-        }
-        if (i < j)
-            arr[j--] = arr[i];
-
+class MaxHeap{
+    vector<int> heap;
+    int size;
+    int parent(int i){
+        return i/2;
     }
-    arr[i] = key;
-    QuickSort(arr, start, i - 1);
-    QuickSort(arr, i + 1, end);
-}
-
-
-int main() {
-    int array[10] = {5, 3, 9, 16, 28, 0, 4, 7, 29, 44};
-    QuickSort(array, 0, 9);
-    for (int i = 0; i < 9; i++) {
-        cout << array[i] << endl;
+    int left_child(int i){
+        return 2*i;
     }
+    int right_child(int i){
+        return 2*i+1;
+    }
+    void max_heapify(int i){
+        int l = left_child(i);
+        int r = right_child(i);
+        int largest_num;
+        if(l <= size && heap[l] > heap[i])
+            largest_num = l;
+        else
+            largest_num = i;
+        if(r <= size && heap[r] > heap[largest_num])
+            largest_num = r;
+        if(largest_num != i){
+            int temp = heap[i];
+            heap[i] = heap[largest_num];
+            heap[largest_num] = temp;
+            max_heapify(largest_num);
+        }
+    }
+
+public:
+// add method to add an element to the max_heap
+    void Add(int n){
+        heap.push_back(n);
+        size += 1;
+        int index = size-1;
+        int par = parent(index);
+        while(index != 0 && heap[index] > heap[par]){
+            int temp = heap[index];
+            heap[index] = heap[par];
+            heap[par] = temp;
+            index = par;
+            par = parent(index);
+        }
+    }
+
+    void printer(){
+        for(auto item:heap){
+            cout<<item<<",";
+        }
+        cout<<endl;
+    }
+
+};
+
+int main(){
+    MaxHeap heap;
+    heap.Add(10);
+    heap.Add(9);
+    heap.Add(6);
+    heap.printer();
+    heap.Add(8);
+    heap.printer();
+    heap.Add(2);
+    heap.printer();
+    heap.Add(7);
+    heap.printer();
     return 0;
-}
+};
